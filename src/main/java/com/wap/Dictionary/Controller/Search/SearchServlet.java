a48c0f485eb0a67a59b2f7fc9097b74b22889441
Adnan Shehzad <adnanshahzad00003@gmail.com>
2019-11-21 11:53:43 -0600

    Error Resolved when word is not found

--- src/main/java/com/wap/Dictionary/Controller/Search/SearchServlet.java
+++ src/main/java/com/wap/Dictionary/Controller/Search/SearchServlet.java
@@ -32,6 +32,8 @@ public class SearchServlet extends HttpServlet {
         String searchword=request.getParameter("searchkeyword");
         JSONObject jsondatabase=searchDao.searchWordFromDatabase(searchword);
         System.out.println("Json database value is : "+ jsondatabase);
+        int code=200;
+        JSONObject jsondata=null;
         if(jsondatabase==null) {
             String API_URL = "https://wordsapiv1.p.rapidapi.com/words/" + searchword;
             try {
@@ -39,17 +41,27 @@ public class SearchServlet extends HttpServlet {
                         .header("x-rapidapi-host", "wordsapiv1.p.rapidapi.com")
                         .header("x-rapidapi-key", "22251f27f5msh98b626b0aa00856p148b29jsn59ff32bb0141")
                         .asJson();
-                JSONObject jsondata = response1.getBody().getObject();
-                String json = jsondata.toString();
-                String word = jsondata.getString("word");
-                System.out.println("Json data is " + jsondata);
-                System.out.println("Json word is " + word);
-                HttpSession session=request.getSession();
-                SearchEntity searchEntity = new SearchEntity(word, json,session);
-                request.setAttribute("searchdata", searchEntity);
-                showDatatoSearchJsp(request,response,jsondata);
-                RequestDispatcher rd = request.getRequestDispatcher("/Insert-search");
-                rd.forward(request, response);
+                 code = response1.getStatus();
+                if(code==200) {
+                    jsondata = response1.getBody().getObject();
+                    String json = jsondata.toString();
+                    String word = jsondata.getString("word");
+                    System.out.println("Json data is " + jsondata);
+                    System.out.println("Json word is " + word);
+                    HttpSession session = request.getSession();
+                    SearchEntity searchEntity = new SearchEntity(word, json, session);
+                    request.setAttribute("searchdata", searchEntity);
+                    showDatatoSearchJsp(request, response, jsondata,code);
+                    RequestDispatcher rd = request.getRequestDispatcher("/Insert-search");
+                    rd.forward(request, response);
+                }
+                else
+                {
+                    showDatatoSearchJsp(request, response, jsondata,code);
+                    RequestDispatcher rd = request.getRequestDispatcher("/Search.jsp");
+                    rd.forward(request, response);
+                }
+
 
             /*JSONArray array=jsondata.getJSONArray("results");
             //for(int i=0;i<array.length();i++){
@@ -64,30 +76,38 @@ public class SearchServlet extends HttpServlet {
         }
         else{
             //Show data From Database
-            showDatatoSearchJsp(request,response,jsondatabase);
+            showDatatoSearchJsp(request,response,jsondatabase,code);
             RequestDispatcher rd = request.getRequestDispatcher("/Search.jsp");
             rd.forward(request, response);
         }
     }
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     }
-    public void showDatatoSearchJsp(HttpServletRequest request, HttpServletResponse response,JSONObject obj){
-        JSONArray array=obj.getJSONArray("results");
-        //for(int i=0;i<array.length();i++){
-        JSONObject resultsdata= array.getJSONObject(1);
-        String definition=resultsdata.getString("definition");
-        String word=obj.getString("word");
-        String partofspeech=resultsdata.getString("partOfSpeech");
-        //JSONObject synonmns=obj.getJSONObject("synonyms");
-        //List<String> list = new ArrayList<String>();
+    public void showDatatoSearchJsp(HttpServletRequest request, HttpServletResponse response,JSONObject obj,int code){
+        if (code==404)
+        {
+            request.setAttribute("def", "Word Does,nt Exist in Dictionary. Please try Another One");
+            request.setAttribute("word", "Sorry");
+            request.setAttribute("pos", "");
+        }
+        else {
+            JSONArray array = obj.getJSONArray("results");
+            //for(int i=0;i<array.length();i++){
+            JSONObject resultsdata = array.getJSONObject(1);
+            String definition = resultsdata.getString("definition");
+            String word = obj.getString("word");
+            String partofspeech = resultsdata.getString("partOfSpeech");
+            //JSONObject synonmns=obj.getJSONObject("synonyms");
+            //List<String> list = new ArrayList<String>();
 
-        //syno.append(synonmns.toString());
-        //System.out.println(list.toString());
-        request.setAttribute("def",definition);
-        request.setAttribute("pos",partofspeech);
-        request.setAttribute("word",word);
-        //request.setAttribute("synonymns",synonmns);
-        System.out.println("Definition is :" + definition + " Part of Speech is : " + partofspeech + "Synonmn is: " );
-        // }
+            //syno.append(synonmns.toString());
+            //System.out.println(list.toString());
+            request.setAttribute("def", definition);
+            request.setAttribute("pos", partofspeech);
+            request.setAttribute("word", word);
+            //request.setAttribute("synonymns",synonmns);
+            System.out.println("Definition is :" + definition + " Part of Speech is : " + partofspeech + "Synonmn is: ");
+            // }
+        }
     }
 }
